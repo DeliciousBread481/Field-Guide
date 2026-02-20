a0ec048435c612fc61af90ce18d9b4bef7d667bd
kobber <kobber@users.noreply.github.com>
2026-02-21 00:37:09 +0100

    added "new" text color to config

--- common/src/main/java/com/evandev/fieldguide/config/ModConfig.java
+++ common/src/main/java/com/evandev/fieldguide/config/ModConfig.java
@@ -52,6 +52,7 @@ public class ModConfig {
     public String textColor = "#8A5E3B";
     public String textTitleColor = "#704623";
     public String textMutedColor = "#C7A875";
+    public String textNewColor = "#63B40C";
     public String pageNumberColor = "#C7A875";
     public String listSilhouetteColor = "#DDC69B";
     public String detailsSilhouetteColor = "#DDC69B";
@@ -178,6 +179,8 @@ public class ModConfig {
     public int getTextMutedColorInt() {
         return parseColor(textMutedColor, 0xC7A875);
     }
+
+    public int getTextNewColorInt() { return parseColor(textNewColor, 0x63B40C); }
 
     public int getPageNumberColorInt() {
         return parseColor(pageNumberColor, 0xC7A875);
