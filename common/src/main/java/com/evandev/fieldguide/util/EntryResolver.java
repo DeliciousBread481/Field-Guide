5abdb399e0754ae67523d81fdc0cb8f1b46bbed4
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-20 17:19:39 -0800

    Merge changes

--- common/src/main/java/com/evandev/fieldguide/util/EntryResolver.java
+++ common/src/main/java/com/evandev/fieldguide/util/EntryResolver.java
@@ -178,6 +178,6 @@ public class EntryResolver {
     }
 
     private static boolean isPlant(Block block) {
-        return block instanceof BushBlock || block instanceof LeavesBlock || block instanceof VineBlock || block instanceof CactusBlock || block instanceof SugarCaneBlock || block instanceof WaterlilyBlock || block instanceof StemBlock;
+        return (block instanceof BushBlock && !(block instanceof StemBlock) && !(block instanceof AttachedStemBlock)) || block instanceof LeavesBlock || block instanceof VineBlock || block instanceof CactusBlock || block instanceof SugarCaneBlock || block instanceof WaterlilyBlock || block instanceof StemGrownBlock || block instanceof CoralFanBlock || block instanceof CoralPlantBlock;
     }
 }
