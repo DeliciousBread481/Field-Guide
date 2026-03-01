fa86d873b65e07dba75ec1d69a62f2d358bc3297
evanbones <74276616+evanbones@users.noreply.github.com>
2026-03-01 12:40:49 -0800

    Fix immersive overlays integration

--- common/src/main/java/com/evandev/fieldguide/client/gui/screens/FieldGuideEntryScreen.java
+++ common/src/main/java/com/evandev/fieldguide/client/gui/screens/FieldGuideEntryScreen.java
@@ -141,18 +141,28 @@ public class FieldGuideEntryScreen extends BookScreen {
 
         if (unlocked && !spawnBiomes.isEmpty()) {
             int itemSize = 20;
-            this.addRenderableWidget(new PaginatedGridWidget<>(this.rightPageBounds.left() + 2, this.rightPageBounds.bottom() - 33, this.rightPageBounds.width() - 4, itemSize, 5, itemSize, 0, spawnBiomes, (graphics, item, x, y, mouseX, mouseY) -> {
+
+            List<ResourceLocation> validBiomes = new ArrayList<>();
+            for (ResourceLocation biome : spawnBiomes) {
+                ResourceLocation texture = ResourceLocation.fromNamespaceAndPath(biome.getNamespace(), "textures/immersiveoverlays/" + biome.getPath() + ".png");
+                if (Minecraft.getInstance().getResourceManager().getResource(texture).isPresent()) {
+                    validBiomes.add(biome);
+                }
+            }
+
+            if (validBiomes.isEmpty()) return;
+
+            this.addRenderableWidget(new PaginatedGridWidget<>(this.rightPageBounds.left() + 2, this.rightPageBounds.bottom() - 33, this.rightPageBounds.width() - 4, itemSize, 5, itemSize, 0, validBiomes, (graphics, item, x, y, mouseX, mouseY) -> {
                 ResourceLocation texture = ResourceLocation.fromNamespaceAndPath(item.getNamespace(), "textures/immersiveoverlays/" + item.getPath() + ".png");
 
-                if (Minecraft.getInstance().getResourceManager().getResource(texture).isPresent()) {
-                    boolean mouseOver = Bounds.isMouseOver(mouseX, mouseY, x, y, itemSize, itemSize);
-                    int backgroundOffset = mouseOver ? itemSize : 0;
-                    graphics.blit(Constants.WIDGETS_TEXTURE, x, y, 20, 64 + backgroundOffset, itemSize, itemSize);
-                    int offset = (itemSize - 16) / 2;
-                    graphics.blit(texture, x + offset, y + offset, 0, 0, 16, 16, 16, 16);
-                    if (Bounds.isMouseOver(mouseX, mouseY, x + offset, y + offset, 16, 16)) {
-                        graphics.renderTooltip(this.font, Component.translatable("biome." + item.getNamespace() + "." + item.getPath()), mouseX, mouseY);
-                    }
+                boolean mouseOver = Bounds.isMouseOver(mouseX, mouseY, x, y, itemSize, itemSize);
+                int backgroundOffset = mouseOver ? itemSize : 0;
+                graphics.blit(Constants.WIDGETS_TEXTURE, x, y, 20, 64 + backgroundOffset, itemSize, itemSize);
+                int offset = (itemSize - 16) / 2;
+                graphics.blit(texture, x + offset, y + offset, 0, 0, 16, 16, 16, 16);
+
+                if (Bounds.isMouseOver(mouseX, mouseY, x + offset, y + offset, 16, 16)) {
+                    graphics.renderTooltip(this.font, Component.translatable("biome." + item.getNamespace() + "." + item.getPath()), mouseX, mouseY);
                 }
             }, item -> {
                 if (this.minecraft != null) this.minecraft.setScreen(new FieldGuideScreen("=!" + item, this));
