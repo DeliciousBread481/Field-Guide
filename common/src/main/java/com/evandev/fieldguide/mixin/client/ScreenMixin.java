83cc699cd65534670f0310778443136817f10553
evanbones <74276616+evanbones@users.noreply.github.com>
2026-03-04 00:16:46 -0800

    Fix #26

--- common/src/main/java/com/evandev/fieldguide/mixin/client/ScreenMixin.java
+++ common/src/main/java/com/evandev/fieldguide/mixin/client/ScreenMixin.java
@@ -5,6 +5,7 @@ import com.evandev.fieldguide.client.gui.screens.FieldGuideCategoryScreen;
 import com.evandev.fieldguide.compat.emi.EmiCompat;
 import com.evandev.fieldguide.platform.Services;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,8 +20,11 @@ public class ScreenMixin {
 
     @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
     private void fieldguide$onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (FieldGuideClient.OPEN_GUIDE_KEY.matches(keyCode, scanCode)) {
+        if (((Screen) (Object) this).getFocused() instanceof EditBox) {
+            return;
+        }
 
+        if (FieldGuideClient.OPEN_GUIDE_KEY.matches(keyCode, scanCode)) {
             if (Services.PLATFORM.isModLoaded("emi")) {
                 ItemStack hoveredStack = EmiCompat.getHoveredItem();
 
