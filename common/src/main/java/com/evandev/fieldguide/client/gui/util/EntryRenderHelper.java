1310213d3d00eff764a1057a5de6116967e9b452
evanbones <74276616+evanbones@users.noreply.github.com>
2026-03-11 23:00:24 -0700

    Fix sideways fish (finally!)

--- common/src/main/java/com/evandev/fieldguide/client/gui/util/EntryRenderHelper.java
+++ common/src/main/java/com/evandev/fieldguide/client/gui/util/EntryRenderHelper.java
@@ -5,6 +5,7 @@ import com.evandev.fieldguide.client.ClientFieldGuideManager;
 import com.evandev.fieldguide.client.data.EntryVisual;
 import com.evandev.fieldguide.config.ModConfig;
 import com.evandev.fieldguide.data.CompositeFieldGuideEntry;
+import com.evandev.fieldguide.mixin.accessor.EntityAccessor;
 import com.evandev.fieldguide.util.StructureUtils;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -20,6 +21,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
@@ -148,8 +150,13 @@ public class EntryRenderHelper {
                 entity.attackAnim = 0.0F;
                 entity.oAttackAnim = 0.0F;
 
+                if (entity instanceof WaterAnimal) {
+                    ((EntityAccessor) entity).fieldguide$setWasTouchingWater(true);
+                }
+
                 MultiBufferSource.BufferSource buffers = Minecraft.getInstance().renderBuffers().bufferSource();
                 try {
+                    Minecraft.getInstance().getEntityRenderDispatcher().render(entity, 0, 0, 0, 0.0F, 1.0F, pose, buffers, LightTexture.FULL_BRIGHT);
                     Minecraft.getInstance().getEntityRenderDispatcher().render(entity, 0, 0, 0, 0.0F, 1.0F, pose, buffers, LightTexture.FULL_BRIGHT);
                 } catch (Exception e) {
                     Constants.LOG.error("Failed to render entity in Field Guide: {}", BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()), e);
