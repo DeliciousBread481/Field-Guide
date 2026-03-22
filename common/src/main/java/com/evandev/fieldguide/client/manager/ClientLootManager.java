f3f8f7a22eadf5e053232d2d87ce9a0af5b9338e
evanbones <74276616+evanbones@users.noreply.github.com>
2026-03-22 14:30:35 -0700

    Fix cobblemon drops not showing up in search

--- common/src/main/java/com/evandev/fieldguide/client/manager/ClientLootManager.java
+++ common/src/main/java/com/evandev/fieldguide/client/manager/ClientLootManager.java
@@ -2,6 +2,7 @@ package com.evandev.fieldguide.client.manager;
 
 import com.evandev.fieldguide.api.AutoPopulateRegistry;
 import com.evandev.fieldguide.api.GuideEntry;
+import com.evandev.fieldguide.compat.cobblemon.FieldGuideCobblemonCompat;
 import com.evandev.fieldguide.network.RequestLootPacket;
 import com.evandev.fieldguide.platform.Services;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -85,6 +86,10 @@ public class ClientLootManager {
                     requestLoot(id);
                 }
             }
+        }
+
+        if (Services.PLATFORM.isModLoaded("cobblemon")) {
+            rawDrops.addAll(FieldGuideCobblemonCompat.getCobblemonDrops(entry));
         }
 
         List<ItemStack> distinct = new ArrayList<>();
