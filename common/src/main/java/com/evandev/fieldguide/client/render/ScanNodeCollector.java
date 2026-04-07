9a9e3100005b9f7922c3c09f91212006310b5ff6
evanbones <74276616+evanbones@users.noreply.github.com>
2026-04-07 03:59:09 -0700

    refactor scannodecollector

---
+++ common/src/main/java/com/evandev/fieldguide/client/render/ScanNodeCollector.java
@@ -0,0 +1,103 @@
+package com.evandev.fieldguide.client.render;
+
+import com.evandev.fieldguide.client.ModRenderTypes;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.OrderedSubmitNodeCollector;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.MovingBlockRenderState;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
+import net.minecraft.client.renderer.block.dispatch.BlockStateModelPart;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.geometry.BakedQuad;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Quaternionf;
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+
+record ScanNodeCollector(SubmitNodeCollector delegate, float r, float g, float b, float a, float limitY,
+                         boolean isDepth) implements SubmitNodeCollector {
+
+    @Override
+    public @NonNull OrderedSubmitNodeCollector order(int order) {
+        return this;
+    }
+
+    @Override
+    public void submitBlockModel(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull List<BlockStateModelPart> parts, int @NonNull [] tintLayers, int lightCoords, int overlayCoords, int outlineColor) {
+        RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
+        delegate.submitBlockModel(poseStack, wrapped, parts, tintLayers, ScanOverlayRenderer.getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, outlineColor);
+    }
+
+    @Override
+    public <S> void submitModel(@NonNull Model<? super S> model, S state, @NonNull PoseStack poseStack, @NonNull RenderType renderType, int lightCoords, int overlayCoords, int tintedColor, TextureAtlasSprite sprite, int outlineColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay) {
+        RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
+        delegate.submitModel(model, state, poseStack, wrapped, ScanOverlayRenderer.getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, tintedColor, sprite, outlineColor, crumblingOverlay);
+    }
+
+    @Override
+    public void submitModelPart(@NonNull ModelPart modelPart, @NonNull PoseStack poseStack, @NonNull RenderType renderType, int lightCoords, int overlayCoords, TextureAtlasSprite sprite, boolean sheeted, boolean hasFoil, int tintedColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay, int outlineColor) {
+        RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
+        delegate.submitModelPart(modelPart, poseStack, wrapped, ScanOverlayRenderer.getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, sprite, sheeted, hasFoil, tintedColor, crumblingOverlay, outlineColor);
+    }
+
+    @Override
+    public void submitShadow(@NonNull PoseStack poseStack, float radius, @NonNull List<EntityRenderState.ShadowPiece> pieces) {
+        delegate.submitShadow(poseStack, radius, pieces);
+    }
+
+    @Override
+    public void submitNameTag(@NonNull PoseStack poseStack, Vec3 nameTagAttachment, int offset, @NonNull Component name, boolean seeThrough, int lightCoords, double distanceToCameraSq, @NonNull CameraRenderState camera) {
+        delegate.submitNameTag(poseStack, nameTagAttachment, offset, name, seeThrough, lightCoords, distanceToCameraSq, camera);
+    }
+
+    @Override
+    public void submitText(@NonNull PoseStack poseStack, float x, float y, net.minecraft.util.@NonNull FormattedCharSequence string, boolean dropShadow, net.minecraft.client.gui.Font.@NonNull DisplayMode displayMode, int lightCoords, int color, int backgroundColor, int outlineColor) {
+        delegate.submitText(poseStack, x, y, string, dropShadow, displayMode, lightCoords, color, backgroundColor, outlineColor);
+    }
+
+    @Override
+    public void submitFlame(@NonNull PoseStack poseStack, @NonNull EntityRenderState renderState, @NonNull Quaternionf rotation) {
+        delegate.submitFlame(poseStack, renderState, rotation);
+    }
+
+    @Override
+    public void submitLeash(@NonNull PoseStack poseStack, EntityRenderState.@NonNull LeashState leashState) {
+        delegate.submitLeash(poseStack, leashState);
+    }
+
+    @Override
+    public void submitMovingBlock(@NonNull PoseStack poseStack, @NonNull MovingBlockRenderState movingBlockRenderState) {
+        delegate.submitMovingBlock(poseStack, movingBlockRenderState);
+    }
+
+    @Override
+    public void submitBreakingBlockModel(@NonNull PoseStack poseStack, @NonNull BlockStateModel model, long seed, int progress) {
+        delegate.submitBreakingBlockModel(poseStack, model, seed, progress);
+    }
+
+    @Override
+    public void submitItem(@NonNull PoseStack poseStack, @NonNull ItemDisplayContext itemDisplayContext, int i, int i1, int i2, int @NonNull [] ints, @NonNull List<BakedQuad> list, ItemStackRenderState.@NonNull FoilType foilType) {
+        delegate.submitItem(poseStack, itemDisplayContext, i, i1, i2, ints, list, foilType);
+    }
+
+    @Override
+    public void submitCustomGeometry(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull CustomGeometryRenderer customGeometryRenderer) {
+        delegate.submitCustomGeometry(poseStack, renderType, customGeometryRenderer);
+    }
+
+    @Override
+    public void submitParticleGroup(@NonNull ParticleGroupRenderer particleGroupRenderer) {
+        delegate.submitParticleGroup(particleGroupRenderer);
+    }
+}
+
--- common/src/main/java/com/evandev/fieldguide/client/render/ScanOverlayRenderer.java
+++ common/src/main/java/com/evandev/fieldguide/client/render/ScanOverlayRenderer.java
@@ -3,7 +3,6 @@ package com.evandev.fieldguide.client.render;
 import com.evandev.fieldguide.ModTags;
 import com.evandev.fieldguide.api.GuideEntry;
 import com.evandev.fieldguide.client.ClientFieldGuideManager;
-import com.evandev.fieldguide.client.ModRenderTypes;
 import com.evandev.fieldguide.client.scan.FieldGuideScanner;
 import com.evandev.fieldguide.compat.etf.EtfCompat;
 import com.evandev.fieldguide.config.ClientConfig;
@@ -11,46 +10,30 @@ import com.evandev.fieldguide.platform.Services;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.Model;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.OrderedSubmitNodeCollector;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.block.BlockModelRenderState;
 import net.minecraft.client.renderer.block.BlockModelResolver;
-import net.minecraft.client.renderer.block.MovingBlockRenderState;
-import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
-import net.minecraft.client.renderer.block.dispatch.BlockStateModelPart;
 import net.minecraft.client.renderer.block.model.BlockDisplayContext;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
-import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
-import net.minecraft.client.renderer.item.ItemStackRenderState;
-import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.geometry.BakedQuad;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragonPart;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import org.joml.Quaternionf;
-import org.jspecify.annotations.NonNull;
 
 import java.awt.*;
 import java.util.*;
-import java.util.List;
 import java.util.Queue;
 
 public class ScanOverlayRenderer {
@@ -349,80 +332,4 @@ public class ScanOverlayRenderer {
         if (isEtfLoaded) EtfCompat.allowRenderLayerTextureModify();
     }
 
-    private record ScanNodeCollector(SubmitNodeCollector delegate, float r, float g, float b, float a, float limitY,
-                                     boolean isDepth) implements SubmitNodeCollector {
-
-        @Override
-        public @NonNull OrderedSubmitNodeCollector order(int order) {
-            return this;
-        }
-
-        @Override
-        public void submitBlockModel(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull List<BlockStateModelPart> parts, int @NonNull [] tintLayers, int lightCoords, int overlayCoords, int outlineColor) {
-            RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
-            delegate.submitBlockModel(poseStack, wrapped, parts, tintLayers, getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, outlineColor);
-        }
-
-        @Override
-        public <S> void submitModel(@NonNull Model<? super S> model, S state, @NonNull PoseStack poseStack, @NonNull RenderType renderType, int lightCoords, int overlayCoords, int tintedColor, TextureAtlasSprite sprite, int outlineColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay) {
-            RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
-            delegate.submitModel(model, state, poseStack, wrapped, getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, tintedColor, sprite, outlineColor, crumblingOverlay);
-        }
-
-        @Override
-        public void submitModelPart(@NonNull ModelPart modelPart, @NonNull PoseStack poseStack, @NonNull RenderType renderType, int lightCoords, int overlayCoords, TextureAtlasSprite sprite, boolean sheeted, boolean hasFoil, int tintedColor, ModelFeatureRenderer.CrumblingOverlay crumblingOverlay, int outlineColor) {
-            RenderType wrapped = isDepth ? ModRenderTypes.wrapForDepth(renderType) : ModRenderTypes.wrapForScan(renderType);
-            delegate.submitModelPart(modelPart, poseStack, wrapped, getPackedScanLightCoords(limitY, r, g, b, a), overlayCoords, sprite, sheeted, hasFoil, tintedColor, crumblingOverlay, outlineColor);
-        }
-
-        @Override
-        public void submitShadow(@NonNull PoseStack poseStack, float radius, @NonNull List<EntityRenderState.ShadowPiece> pieces) {
-            delegate.submitShadow(poseStack, radius, pieces);
-        }
-
-        @Override
-        public void submitNameTag(@NonNull PoseStack poseStack, Vec3 nameTagAttachment, int offset, @NonNull Component name, boolean seeThrough, int lightCoords, double distanceToCameraSq, @NonNull CameraRenderState camera) {
-            delegate.submitNameTag(poseStack, nameTagAttachment, offset, name, seeThrough, lightCoords, distanceToCameraSq, camera);
-        }
-
-        @Override
-        public void submitText(@NonNull PoseStack poseStack, float x, float y, net.minecraft.util.@NonNull FormattedCharSequence string, boolean dropShadow, net.minecraft.client.gui.Font.@NonNull DisplayMode displayMode, int lightCoords, int color, int backgroundColor, int outlineColor) {
-            delegate.submitText(poseStack, x, y, string, dropShadow, displayMode, lightCoords, color, backgroundColor, outlineColor);
-        }
-
-        @Override
-        public void submitFlame(@NonNull PoseStack poseStack, @NonNull EntityRenderState renderState, @NonNull Quaternionf rotation) {
-            delegate.submitFlame(poseStack, renderState, rotation);
-        }
-
-        @Override
-        public void submitLeash(@NonNull PoseStack poseStack, EntityRenderState.@NonNull LeashState leashState) {
-            delegate.submitLeash(poseStack, leashState);
-        }
-
-        @Override
-        public void submitMovingBlock(@NonNull PoseStack poseStack, @NonNull MovingBlockRenderState movingBlockRenderState) {
-            delegate.submitMovingBlock(poseStack, movingBlockRenderState);
-        }
-
-        @Override
-        public void submitBreakingBlockModel(@NonNull PoseStack poseStack, @NonNull BlockStateModel model, long seed, int progress) {
-            delegate.submitBreakingBlockModel(poseStack, model, seed, progress);
-        }
-
-        @Override
-        public void submitItem(@NonNull PoseStack poseStack, @NonNull ItemDisplayContext itemDisplayContext, int i, int i1, int i2, int @NonNull [] ints, @NonNull List<BakedQuad> list, ItemStackRenderState.@NonNull FoilType foilType) {
-            delegate.submitItem(poseStack, itemDisplayContext, i, i1, i2, ints, list, foilType);
-        }
-
-        @Override
-        public void submitCustomGeometry(@NonNull PoseStack poseStack, @NonNull RenderType renderType, @NonNull CustomGeometryRenderer customGeometryRenderer) {
-            delegate.submitCustomGeometry(poseStack, renderType, customGeometryRenderer);
-        }
-
-        @Override
-        public void submitParticleGroup(@NonNull ParticleGroupRenderer particleGroupRenderer) {
-            delegate.submitParticleGroup(particleGroupRenderer);
-        }
-    }
 }
